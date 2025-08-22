fa1f28506a42cd02aad9f2b22f30c9255f68971b
simongs1018 <simongomez@ucundinamarca.edu.co>
2025-08-22 17:12:18 -0500

    primeros datos

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hola mundo");
+        System.out.printf("Simon Gomez Sanabria");
+        System.out.printf("Estudiatee");
+
+    }
+}
